7d81e3cfb7ca464c21d9822392d4fa5d5e200c9b
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-12-09 16:22:16 +0530

    feat: CORS middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,12 +7,22 @@ import (
 	"github.com/ctrixcode/go-chi-postgres/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	r.Get("/", handlers.HelloWorldHandler)
 	r.Get("/health", s.healthHandler)
